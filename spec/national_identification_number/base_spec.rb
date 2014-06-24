c31665145c66c6a0d4165aca717b743389692416
mmzoo <b@noteyard.com>
2014-06-24 12:19:57 +0200

    Rspec should -> expect

--- spec/national_identification_number/base_spec.rb
+++ spec/national_identification_number/base_spec.rb
@@ -1,74 +1,77 @@
 require 'spec_helper'
-require 'resident'
-
-include NationalIdentificationNumber
+require 'national_identification_number/base'
 
 class TestBaseChild < NationalIdentificationNumber::Base
   def set_valid_to_nil
     @valid = nil
   end
+
   protected
+
   def validate
     @valid = 'validates_to_true'
   end
 end
 
-describe Base, 'new' do
+describe NationalIdentificationNumber::Base do
 
-  it "should instantiate correctly" do
-    TestBaseChild.new('12345').should be_an_instance_of TestBaseChild
+  describe '#initialize' do
+
+    it "instantiates correctly" do
+      expect( TestBaseChild.new('12345') ).to be_an_instance_of TestBaseChild
+    end
+
+    it "repairs on initialization" do
+      expect( TestBaseChild.new(' 12345abc ').to_s ).to eq '12345ABC'
+    end
+
+    it "validates on initialization" do
+      expect { NationalIdentificationNumber::Base.new('12345') }.to raise_error(RuntimeError, 'You need to implement at least this method in subclasses.')
+    end
+
   end
 
- it "should repair on initialization" do
-   TestBaseChild.new(' 12345abc ').to_s.should == '12345ABC'
- end
+  describe '#valid?' do
 
- it "should validate on initialization" do
-   lambda {  NationalIdentificationNumber::Base.new('12345') }.should raise_error(RuntimeError, 'You need to implement at least this method in subclasses.')
- end
+    it "should return true if it is valid" do
+      expect( TestBaseChild.new('12345abc') ).to be_valid
+    end
 
-end
+    it "should return true if it is valid" do
+      base = TestBaseChild.new('12345abc')
+      base.set_valid_to_nil
+      expect( base ).to_not be_valid
+    end
 
-describe Base, 'valid?' do
-
-  it "should return true if it is valid" do
-    TestBaseChild.new('12345abc').should be_valid
   end
 
-  it "should return true if it is valid" do
-    base = TestBaseChild.new('12345abc')
-    base.set_valid_to_nil
-    base.should_not be_valid
+  describe '#to_s' do
+
+    it "should return the number" do
+      expect( TestBaseChild.new('12345ABC').to_s ).to eq '12345ABC'
+    end
+
   end
 
-end
+  describe '#repair' do
 
-describe Base, 'to_s' do
+    it "should be a protected method" do
+      expect { TestBaseChild.new('12345').repair }.to raise_error NoMethodError
+    end
 
-  it "should return the number" do
-    TestBaseChild.new('12345ABC').to_s.should == '12345ABC'
-  end
+    it "should strip whitespaces and capitalize everything" do
+      expect( TestBaseChild.new(' 123  45  abc %&/ ').to_s ).to eq '12345ABC%&/'
+    end
 
-end
+    it "should make it a string" do
+      expect( TestBaseChild.new(:symbol).to_s ).to eq 'SYMBOL'
+      expect( TestBaseChild.new(1234).to_s ).to eq '1234'
+      expect( TestBaseChild.new(nil).to_s ).to eq ''
+    end
 
-describe Base, 'repair' do
-
-  it "should be a protected method" do
-    lambda { TestBaseChild.new('12345').repair }.should raise_error NoMethodError
-  end
-
-  it "should strip whitespaces and capitalize everything" do
-    TestBaseChild.new(' 123  45  abc %&/ ').to_s.should == '12345ABC%&/'
-  end
-
-  it "should make it a string" do
-    TestBaseChild.new(:symbol).to_s.should == 'SYMBOL'
-    TestBaseChild.new(1234).to_s.should == '1234'
-    TestBaseChild.new(nil).to_s.should == ''
-  end
-
-  it "should strip newlines and tabs" do
-    TestBaseChild.new("one\ntwo\tthree\n").to_s.should == 'ONETWOTHREE'
+    it "should strip newlines and tabs" do
+      expect( TestBaseChild.new("one\ntwo\tthree\n").to_s ).to eq 'ONETWOTHREE'
+    end
   end
 
 end
--- spec/national_identification_number/swedish_spec.rb
+++ spec/national_identification_number/swedish_spec.rb
@@ -1,147 +1,146 @@
 require 'spec_helper'
-require 'resident'
+require 'national_identification_number/swedish'
 
 include NationalIdentificationNumber
 
 describe Swedish, '.generate' do
 
-  it "should generate valid numbers" do
-    50.times { Swedish.new(Swedish.generate).should be_valid } # I know, it feels weird :)
+  it "generates valid numbers" do
+    50.times { expect( Swedish.new(Swedish.generate) ).to be_valid } # I know, it feels weird :)
   end
 
-end
+  describe '#valid?' do
 
-describe Swedish, 'valid?' do
+    it "recognizes valid numbers" do
+      expect( Swedish.new("19180123-2669") ).to be_valid
+      expect( Swedish.new("00180123-2669") ).to be_valid
+      expect( Swedish.new("000180123-2669") ).to be_valid
+      expect( Swedish.new("050126-1853") ).to be_valid
+      expect( Swedish.new("0asdfghj501261853") ).to be_valid
+      expect( Swedish.new("050112-2451") ).to be_valid
+      expect( Swedish.new("450202-6950") ).to be_valid
+      expect( Swedish.new("19450202-6950") ).to be_valid
+    end
 
-  it "should recognize valid numbers" do
-    Swedish.new("19180123-2669").should be_valid
-    Swedish.new("00180123-2669").should be_valid
-    Swedish.new("000180123-2669").should be_valid
-    Swedish.new("050126-1853").should be_valid
-    Swedish.new("0asdfghj501261853").should be_valid
-    Swedish.new("050112-2451").should be_valid
-    Swedish.new("450202-6950").should be_valid
-    Swedish.new("19450202-6950").should be_valid
+    it "recognizes invalid numbers" do
+      expect( Swedish.new("991301-1236") ).to_not be_valid # valid checksum, invalid month
+      expect( Swedish.new("830231-5554") ).to_not be_valid # valid checksum, invalid day
+      expect( Swedish.new("050112--2451") ).to_not be_valid
+      expect( Swedish.new("123456-1239") ).to_not be_valid
+      expect( Swedish.new("180123-2668") ).to_not be_valid
+      expect( Swedish.new("150D1261853") ).to_not be_valid
+      expect( Swedish.new("750112-2451") ).to_not be_valid
+      expect( Swedish.new("123") ).to_not be_valid
+      expect( Swedish.new("000000-0000") ).to_not be_valid
+      expect( Swedish.new("0000000000") ).to_not be_valid
+      expect( Swedish.new("asdfghj") ).to_not be_valid
+      expect( Swedish.new("") ).to_not be_valid
+      expect( Swedish.new(12345678) ).to_not be_valid
+      expect( Swedish.new(:really_bad_input) ).to_not be_valid
+    end
+
   end
 
-  it "should recognize invalid numbers" do
-    Swedish.new("991301-1236").should_not be_valid # valid checksum, invalid month
-    Swedish.new("830231-5554").should_not be_valid # valid checksum, invalid day
-    Swedish.new("050112--2451").should_not be_valid
-    Swedish.new("123456-1239").should_not be_valid
-    Swedish.new("180123-2668").should_not be_valid
-    Swedish.new("150D1261853").should_not be_valid
-    Swedish.new("750112-2451").should_not be_valid
-    Swedish.new("123").should_not be_valid
-    Swedish.new("000000-0000").should_not be_valid
-    Swedish.new("0000000000").should_not be_valid
-    Swedish.new("asdfghj").should_not be_valid
-    Swedish.new("").should_not be_valid
-    Swedish.new(12345678).should_not be_valid
-    Swedish.new(:really_bad_input).should_not be_valid
+  describe '#to_s' do
+
+    it "repair missing dashes and a superfluous century, even if the number is invalid" do
+      expect( Swedish.new('1234561239').to_s ).to eq '123456-1239'
+      expect( Swedish.new('123456-1239').to_s ).to eq '123456-1239'
+      expect( Swedish.new("191234561239").to_s ).to eq '123456-1239'
+      expect( Swedish.new("19123456-1239").to_s ).to eq '123456-1239'
+    end
+
+    it "return the number normalized if the number is valid" do
+      expect( Swedish.new('0501261853').to_s ).to eq '050126-1853'
+      expect( Swedish.new('050126-1853').to_s ).to eq '050126-1853'
+      expect( Swedish.new("190501261853").to_s ).to eq '050126-1853'
+      expect( Swedish.new("19050126-1853").to_s ).to eq '050126-1853'
+      expect( Swedish.new("19050126-185d3").to_s ).to eq '050126-1853'
+      expect( Swedish.new("19050126-185d3\n").to_s ).to eq '050126-1853'
+    end
+
+    it "always be injection safe, valid or not" do
+      expect( Swedish.new("    180 123  -  2669 \n\n\t").to_s ).to eq '180123-2669'
+      expect( Swedish.new("    180 123  -  2669 \n").to_s ).to eq '180123-2669'
+      expect( Swedish.new(%Q{180123-"2669}).to_s ).to eq '180123-2669'
+      expect( Swedish.new(%Q{180123-<2669}).to_s ).to eq '180123-2669'
+      expect( Swedish.new(%Q{1801D23-'2669}).to_s ).to eq '180123-2669'
+      expect( Swedish.new(%Q{180s123->2669}).to_s ).to eq '180123-2669'
+      expect( Swedish.new(%Q{19180s123->2669}).to_s ).to eq '180123-2669'
+    end
+
   end
 
-end
+  describe '#date' do
 
-describe Swedish, 'to_s' do
+    it "recognizes babies born today" do
+      today = Date.today.strftime("%y%m%d")
+      born_today = Swedish.new("#{today}-000#{Swedish.luhn_algorithm("#{today}000")}")
+      expect( born_today ).to be_valid
+      expect( born_today.date).to eq Date.today
+    end
 
-  it "should repair missing dashes and a superfluous century, even if the number is invalid" do
-    Swedish.new('1234561239').to_s.should == '123456-1239'
-    Swedish.new('123456-1239').to_s.should == '123456-1239'
-    Swedish.new("191234561239").to_s.should == '123456-1239'
-    Swedish.new("19123456-1239").to_s.should == '123456-1239'
-  end
+    it "recognizes babies born yesterday" do
+      today = (Date.today - 1).strftime("%y%m%d")
+      born_yesterday = Swedish.new("#{today}-000#{Swedish.luhn_algorithm("#{today}000")}")
+      expect( born_yesterday ).to be_valid
+      expect( born_yesterday.date ).to eq (Date.today - 1)
+    end
 
-  it "should return the number normalized if the number is valid" do
-    Swedish.new('0501261853').to_s.should == '050126-1853'
-    Swedish.new('050126-1853').to_s.should == '050126-1853'
-    Swedish.new("190501261853").to_s.should == '050126-1853'
-    Swedish.new("19050126-1853").to_s.should == '050126-1853'
-    Swedish.new("19050126-185d3").to_s.should == '050126-1853'
-    Swedish.new("19050126-185d3\n").to_s.should == '050126-1853'
-  end
+    it "recognizes people born in the year 2000" do
+      beginning_of_year = Swedish.new("000101-0008")
+      expect( beginning_of_year ).to be_valid
+      expect( beginning_of_year.date ).to eq Date.new(2000, 1, 1)
+      leap_year = Swedish.new("000229-0005") # 2000 was a leap year, 1900 not
+      expect( leap_year ).to be_valid
+      expect( leap_year.date ).to eq Date.new(2000, 2, 29)
+      end_of_year = Swedish.new("001231-0009")
+      expect( end_of_year ).to be_valid
+      expect( end_of_year.date ).to eq Date.new(2000, 12, 31)
+    end
 
-  it "should always be injection safe, valid or not" do
-    Swedish.new("    180 123  -  2669 \n\n\t").to_s.should == '180123-2669'
-    Swedish.new("    180 123  -  2669 \n").to_s.should == '180123-2669'
-    Swedish.new(%Q{180123-"2669}).to_s.should == '180123-2669'
-    Swedish.new(%Q{180123-<2669}).to_s.should == '180123-2669'
-    Swedish.new(%Q{1801D23-'2669}).to_s.should == '180123-2669'
-    Swedish.new(%Q{180s123->2669}).to_s.should == '180123-2669'
-    Swedish.new(%Q{19180s123->2669}).to_s.should == '180123-2669'
-  end
+    it "recognizes people born in the year 1999" do
+      beginning_of_year = Swedish.new("990101-0000")
+      expect( beginning_of_year ).to be_valid
+      expect( beginning_of_year.date ).to eq Date.new(1999, 1, 1)
+      end_of_year = Swedish.new("991231-0001")
+      expect( end_of_year ).to be_valid
+      expect( end_of_year.date ).to eq Date.new(1999, 12, 31)
+    end
 
-end
+    it "recognizes all other people less than 100 years old" do
+      will_turn_99_this_year = Date.new(Date.today.year - 99, 12, 31).strftime("%y%m%d")
+      soon_99 = Swedish.new("#{will_turn_99_this_year}-000#{Swedish.luhn_algorithm("#{will_turn_99_this_year}000")}")
+      expect( soon_99 ).to be_valid
+      expect( soon_99.date ).to eq Date.new(Date.today.year - 99, 12, 31)
+      turned_99_this_year = Date.new(Date.today.year - 99, 1, 1).strftime("%y%m%d")
+      already_99 = Swedish.new("#{turned_99_this_year}-000#{Swedish.luhn_algorithm("#{turned_99_this_year}000")}")
+      expect( already_99 ).to be_valid
+      expect( already_99.date ).to eq Date.new(Date.today.year - 99, 1, 1)
+    end
 
-describe Swedish, 'date' do # For testing only, no public API
+    it "recognizes people that turn 100 years in this year" do
+      will_turn_100_this_year = Date.new(Date.today.year - 100, 12, 31).strftime("%y%m%d")
+      soon_100 = Swedish.new("#{will_turn_100_this_year}+000#{Swedish.luhn_algorithm("#{will_turn_100_this_year}000")}")
+      expect( soon_100 ).to be_valid
+      expect( soon_100.date ).to eq Date.new(Date.today.year - 100, 12, 31)
+      turned_100_this_year = Date.new(Date.today.year - 100, 1, 1).strftime("%y%m%d")
+      already_100 = Swedish.new("#{turned_100_this_year}+000#{Swedish.luhn_algorithm("#{turned_100_this_year}000")}")
+      expect( already_100 ).to be_valid
+      expect( already_100.date ).to eq Date.new(Date.today.year - 100, 1, 1)
+    end
 
-  it "should recognize babies born today" do
-    today = Date.today.strftime("%y%m%d")
-    born_today = Swedish.new("#{today}-000#{Swedish.luhn_algorithm("#{today}000")}")
-    born_today.should be_valid
-    born_today.date.should == Date.today
-  end
+    it "recognizes people older than 100 years born after 1900" do
+      normal = Swedish.new("010101+0007")
+      expect( normal ).to be_valid
+      expect( normal.date ).to eq Date.new(1901, 1, 1)
+    end
 
-  it "should recognize babies born yesterday" do
-    today = (Date.today - 1).strftime("%y%m%d")
-    born_yesterday = Swedish.new("#{today}-000#{Swedish.luhn_algorithm("#{today}000")}")
-    born_yesterday.should be_valid
-    born_yesterday.date.should == (Date.today - 1)
-  end
-
-  it "should recognize people born in the year 2000" do
-    beginning_of_year = Swedish.new("000101-0008")
-    beginning_of_year.should be_valid
-    beginning_of_year.date.should == Date.new(2000, 1, 1)
-    leap_year = Swedish.new("000229-0005") # 2000 was a leap year, 1900 not
-    leap_year.should be_valid
-    leap_year.date.should == Date.new(2000, 2, 29)
-    end_of_year = Swedish.new("001231-0009")
-    end_of_year.should be_valid
-    end_of_year.date.should == Date.new(2000, 12, 31)
-  end
-
-  it "should recognize people born in the year 1999" do
-    beginning_of_year = Swedish.new("990101-0000")
-    beginning_of_year.should be_valid
-    beginning_of_year.date.should == Date.new(1999, 1, 1)
-    end_of_year = Swedish.new("991231-0001")
-    end_of_year.should be_valid
-    end_of_year.date.should == Date.new(1999, 12, 31)
-  end
-
-  it "should recognize all other people less than 100 years old" do
-    will_turn_99_this_year = Date.new(Date.today.year - 99, 12, 31).strftime("%y%m%d")
-    soon_99 = Swedish.new("#{will_turn_99_this_year}-000#{Swedish.luhn_algorithm("#{will_turn_99_this_year}000")}")
-    soon_99.should be_valid
-    soon_99.date.should == Date.new(Date.today.year - 99, 12, 31)
-    turned_99_this_year = Date.new(Date.today.year - 99, 1, 1).strftime("%y%m%d")
-    already_99 = Swedish.new("#{turned_99_this_year}-000#{Swedish.luhn_algorithm("#{turned_99_this_year}000")}")
-    already_99.should be_valid
-    already_99.date.should == Date.new(Date.today.year - 99, 1, 1)
-  end
-
-  it "should recognize people that turn 100 years in this year" do
-    will_turn_100_this_year = Date.new(Date.today.year - 100, 12, 31).strftime("%y%m%d")
-    soon_100 = Swedish.new("#{will_turn_100_this_year}+000#{Swedish.luhn_algorithm("#{will_turn_100_this_year}000")}")
-    soon_100.should be_valid
-    soon_100.date.should == Date.new(Date.today.year - 100, 12, 31)
-    turned_100_this_year = Date.new(Date.today.year - 100, 1, 1).strftime("%y%m%d")
-    already_100 = Swedish.new("#{turned_100_this_year}+000#{Swedish.luhn_algorithm("#{turned_100_this_year}000")}")
-    already_100.should be_valid
-    already_100.date.should == Date.new(Date.today.year - 100, 1, 1)
-  end
-
-  it "should recognize people older than 100 years born after 1900" do
-    normal = Swedish.new("010101+0007")
-    normal.should be_valid
-    normal.date.should == Date.new(1901, 1, 1)
-  end
-
-  it "should recognize people older than 100 years born before 1900" do
-    normal = Swedish.new("991231+0001")
-    normal.should be_valid
-    normal.date.should == Date.new(1899, 12, 31)
+    it "recognizes people older than 100 years born before 1900" do
+      normal = Swedish.new("991231+0001")
+      expect( normal ).to be_valid
+      expect( normal.date ).to eq Date.new(1899, 12, 31)
+    end
   end
 
 end
